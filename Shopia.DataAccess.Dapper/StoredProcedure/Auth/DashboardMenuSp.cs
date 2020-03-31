2b3793e9da763a27ae91803d11df510ae8220cc3
Mehran <mehrannoruzi@gmail.com>
2020-03-31 17:33:46 +0430

    Add DataAccess.Dapper Layer

---
+++ Shopia.DataAccess.Dapper/StoredProcedure/Auth/DashboardMenuSp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Elk.Dapper;
+using Shopia.Domain;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Shopia.DataAccess.Dapper
+{
+    public class DashboardMenuSp
+    {
+        private SqlConnection _sqlConnection;
+
+        public DashboardMenuSp(string connectionString)
+        {
+            _sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<MenuSPModel> GetUserMenu(Guid userId)
+            => _sqlConnection.ExecuteSpList<MenuSPModel>("[Auth].[GetUserMenu]", new { UserId = userId });
+    }
+}
