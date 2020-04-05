efeec0b67247c70076342dd103621e5c2c3c8c88
dotbydot <dot.dotbydot@gmail.com>
2020-04-05 10:55:29 +0430

    * Discount.cs: remove discount class model from domain root.
    * Shopia.Domain.csproj:

---
+++ Shopia.Domain/Entity/Discount.cs
@@ -0,0 +1,42 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Shopia.Domain.Enum;
+
+namespace Shopia.Domain
+{
+    public class Discount
+    {
+        public int DiscountId { get; set; }
+
+          public DiscountType DiscountType { get; set; }
+        
+          public int Value { get; set; }
+
+         public int Ceiling { get; set; }
+
+        public bool IsUsed { get; set; }
+
+        /// <summary>
+        ///  تعداد مصرف      
+        /// </summary>
+        public bool IsDisposable { get; set; }
+
+        /// <summary>
+        /// تعداد کاربر
+        /// </summary>
+         public bool ForFirstUser { get; set; }
+
+         public DateTime ValidFromDateMi { get; set; }
+
+         public DateTime ValidToDateMi { get; set; }
+
+        [Column(TypeName = "char")]
+           public string ValidFromDateSh { get; set; }
+
+        [Column(TypeName = "char")]
+           public string ValidToDateSh { get; set; }
+
+        [Column(TypeName = "varchar")]
+           public string Code { get; set; }
+    }
+}
