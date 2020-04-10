fade04e05788591de8998dfc11bdf83fa00bd936
Mehran <mehrannoruzi@gmail.com>
2020-04-10 18:03:27 +0430

    Add Crawler.DataAccess.Ef Layer

---
+++ Shopia.Crawler.DataAccess.Ef/Context/CrawlerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Shopia.Domain;
+using Elk.EntityFrameworkCore;
+using Elk.EntityFrameworkCore.Tools;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopia.DataAccess.Ef
+{
+    public class CrawlerDbContext : ElkDbContext
+    {
+        public CrawlerDbContext() { }
+
+        public CrawlerDbContext(DbContextOptions<CrawlerDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Page>().HasIndex(x => x.Username).HasName("IX_Username").IsUnique();
+            builder.Entity<Post>().HasIndex(x => x.UniqueId).HasName("IX_UniqueId").IsUnique();
+            builder.Entity<PostAsset>().HasIndex(x => x.UniqueId).HasName("IX_UniqueId").IsUnique();
+
+            builder.OverrideDeleteBehavior();
+            builder.RegisterAllEntities<ICrawlerEntity>(typeof(Page).Assembly);
+        }
+    }
+}
