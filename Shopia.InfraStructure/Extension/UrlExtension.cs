897b80e65721f7beb46307d1b990c3e0d6d09a4f
Mehran <mehrannoruzi@gmail.com>
2020-04-10 17:24:01 +0430

    Add InfraStructure CrossCutting Layer

---
+++ Shopia.InfraStructure/Extension/UrlExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Shopia.InfraStructure
+{
+    public static class UrlExtension
+    {
+        public static string ToEncodedUrl(this string url)
+            => WebUtility.UrlEncode(url);
+
+        public static string ToDecodedUrl(this string url)
+            => WebUtility.UrlDecode(url);
+
+    }
+}
---
+++ Shopia.InfraStructure/Tools/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shopia.InfraStructure
+{
+    public class Parser
+    {
+        public static string ToString(object obj) => obj == null ? string.Empty : obj.ToString().Trim().Replace("{", "").Replace("}", "");
+
+        public static int ToInt(object obj) => int.Parse(ToString(obj));
+
+        public static bool ToBool(object obj) => bool.Parse(ToString(obj));
+
+        public static DateTime ToDateTime(double timeStamp, bool isUnixTimeStamp = true)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            if (isUnixTimeStamp)
+            {
+                // Unix timestamp is seconds past epoch
+                dateTime = dateTime.AddSeconds(timeStamp).ToLocalTime();
+            }
+            else
+            {
+                // Java timestamp is AddMilliseconds past epoch
+                dateTime = dateTime.AddMilliseconds(timeStamp).ToLocalTime();
+            }
+
+            return dateTime;
+        }
+    }
+}
