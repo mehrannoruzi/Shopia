713700d7fe373fed1b4541463e64342e5e5bd804
m.norouzi <m.norouzi@NOROUZI-PC>
2020-05-06 14:21:51 +0430

    Add Global Variables

---
+++ Shopia.InfraStructure/Variable/GlobalVariables.cs
@@ -0,0 +1,24 @@
+﻿namespace Shopia.InfraStructure
+{
+    public static class GlobalVariables
+    {
+        public static class SmsProviders
+        {
+            public static class LinePayamak
+            {
+                public static string Username = "80225353";
+                public static string Password = "500096998998";
+                public static string SenderId = "500096998998";
+            }
+        }
+
+        public static class DeliveryProviders
+        {
+            public static class AloPeik
+            {
+                public static string Url = "https://sandbox-api.alopeyk.com/api/v2/";
+                public static string Token = "";
+            }
+        }
+    }
+}
